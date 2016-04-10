dc9c096a01646ee7a5c569fbce82de3da624b7d4
fahad-gcet <fms525@outlook.com>
2016-04-10 18:35:37 +0000

    Sorted by DESC

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order('created_at DESC')
   end
 
   # GET /events/1
